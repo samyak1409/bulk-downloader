0704aa363d61e3dd4eb6cd1595fea072064f7f24
Samyak Jain <samyak65400@gmail.com>
2021-09-22 05:11:21 +0530

    added ddos, js notif, and many improvements

--- Script.py
+++ Script.py
@@ -17,6 +17,20 @@ TITLE = 'BULK DOWNLOADER (USING WEB SCRAPING)'
 HTTPS, HTTP = 'https://', 'http://'
 NA = 'n/a'
 DL_DIR = 'Downloads'  # download dir
+anti_ddos = ('https://ddos-guard.net',
+             'https://deflect.ca',
+             'https://jigsaw.google.com',
+             'https://kemptechnologies.com',
+             'https://www.akamai.com',
+             'https://www.blockdos.net',
+             'https://www.cloudflare.com',
+             'https://www.f5.com',
+             'https://www.fastly.com',
+             'https://www.imperva.com',
+             'https://www.netscout.com',
+             'https://www.radware.com',
+             'https://www.stackpath.com',
+             'https://www.verisign.com')  # URLs of DDoS (distributed denial-of-service) mitigation companies
 
 
 # FUNCTIONS:
@@ -26,6 +40,14 @@ def sep_print(*msgs):
     if msgs:
         print(*msgs)
     print('-' * len(TITLE))
+
+
+def get_file_name(s: str):
+    """"""
+    name = ''
+    for char in s:
+        name += char if char not in ('\\', '/', ':', '*', '?', '"', '<', '>', '|') else ' '  # a file name can't contain any of these characters
+    return name
 
 
 def download(lnk: str, cont: str, i_str: str, smart: bool = True):
@@ -38,7 +60,7 @@ def download(lnk: str, cont: str, i_str: str, smart: bool = True):
 
     try:
         response = get_request(url=lnk, stream=True)
-        # print(response.headers, '\n'); continue  # debugging
+        # print(response.headers, '\n')  # debugging
     except RequestException as re:
         print('ERROR:', re.__doc__, '\n')
         return 0  # skipping this link for any other further processing
@@ -48,14 +70,25 @@ def download(lnk: str, cont: str, i_str: str, smart: bool = True):
     print('Content Type:', content_type)
     print('Size (bytes):', size)
 
-    filename = response.headers.get('Content-Disposition')
-
     # Download Process:
 
-    if filename:  # downloading only if filename is there
+    if 'html' not in content_type:  # downloading if content is not html
 
-        filename = filename[filename.find('filename=') + 9:].split(sep='"', maxsplit=2)[1]  # extracting filename
-        # print(filename); continue  # debugging
+        # Extracting filename:
+
+        filename = response.headers.get('Content-Disposition', NA)
+        f_index = filename.find('filename=')
+        if f_index != -1:
+            filename = filename[f_index+9:].split(sep='"', maxsplit=2)[1]  # 'attachment;filename="example.ext"' -> 'example.ext'
+        else:
+            name = lnk.rsplit(sep='/', maxsplit=1)[1]  # last component of link
+            if name.startswith(('?', '&')):  # URL's parameters starting
+                name = lnk.rsplit(sep='/', maxsplit=2)[1]
+            else:
+                name = name.split(sep='&', maxsplit=1)[0]  # excluding extra parameters
+            name += '.' + content_type.split(sep='/', maxsplit=1)[1].split(sep=';', maxsplit=1)[0]  # extension
+            filename = get_file_name(s=name)
+        # print(filename, '\n'); return 0  # debugging
 
         # Main Downloading:
 
@@ -68,6 +101,7 @@ def download(lnk: str, cont: str, i_str: str, smart: bool = True):
 
         except Exception as ex:  # any non-exit exception
             print('ERROR OCCURRED:', ex, '\n')
+            response.close()
             return 0
 
         print('Download: Completed \n')
@@ -75,6 +109,7 @@ def download(lnk: str, cont: str, i_str: str, smart: bool = True):
         if smart and lnk not in downloaded:  # uniqueness
             insort_right(a=downloaded, x=lnk)
 
+        response.close()
         return 1
 
     else:
@@ -83,7 +118,14 @@ def download(lnk: str, cont: str, i_str: str, smart: bool = True):
         if smart and lnk not in skipped:  # uniqueness
             insort_right(a=skipped, x=(lnk, cont))
 
+        response.close()
         return 0
+
+
+def partitioned(lnk: str):  # 'https://example.com/'
+    """"""
+    p = lnk.split('/')[2:]  # ['https:', '', 'example.com', ''] -> ['example.com', '']
+    return p if p[-1] else p[:-1]  # ['example.com', ''] -> ['example.com']
 
 
 # MAIN:
@@ -110,10 +152,10 @@ while True:
     else:
         if not (url.startswith(HTTPS) or url.startswith(HTTP)):
             url = HTTPS + url
-        if url.endswith('/'):  # https://unsplash.com/ -> https://unsplash.com
+        if url.endswith('/'):  # https://example.com/ -> https://example.com
             url = url[:-1]
 
-    print(f'\nLinks from "{url}" are being extracted... \n')  # loading message
+    print(f'\nLinks from "{url}" are being extracted... \n')
 
     # PARSING LINKS:
 
@@ -137,8 +179,25 @@ while True:
 
             data[link.strip()] = content.strip()
 
-    data_len = len(data)
-    print('Total Links Found:', data_len)
+    total_data = len(data)
+
+    if total_data == 1:
+
+        link_ = list(data)[0]
+
+        # Anti-DDoS Detection:
+        try:
+            sep_print(f'OOPS! Entered URL is protected from bots by "{next(filter(lambda x: link_.startswith(x), anti_ddos))}". Please try entering some other website.')
+        except StopIteration:
+            pass
+        else:
+            continue
+
+        if link_.startswith('http://enable-javascript.com'):
+            sep_print("OOPS! JavaScript webpage. Sorry I can't run JavaScript webpages, please try some other website.")
+            continue
+
+    print('Total Links Found:', total_data)
     # print(); list(map(print, data.items()))  # debugging
 
     if not data:  # no links found
@@ -153,41 +212,26 @@ while True:
 
     # OUTPUT PROCESS:
 
-    dir_name = ''
-    for char in url[url.index('//')+2:]:
-        dir_name += char if char not in ('\\', '/', ':', '*', '?', '"', '<', '>', '|') else ' '  # a file name can't contain any of those characters
+    dir_name = get_file_name(s=url[url.index('//')+2:])  # (URL without protocol)
     dir_name += f" ({str(datetime.now().time()).replace(':', ';')}) "  # appending current time to make dir_name distinct
     mkdir(dir_name)
     chdir(dir_name)
     # print(dir_name); exit()  # debugging
 
-    homepage = '/'.join(url.split(sep='/', maxsplit=3)[:3])
+    homepage = '/'.join(url.split(sep='/', maxsplit=3)[:3])  # URL = 'http://www.example.com/index.html'; homepage = 'http://www.example.com'
     # print("URL's Homepage:", homepage, '\n')  # debugging
     count = 0
     downloaded, skipped = [], []  # downloaded links, skipped (link, content) tuple
 
     for i, (link, content) in enumerate(iterable=data.items(), start=1):  # iterating over fetched links
+        count += download(homepage+link if link.startswith(('/', '#')) else link, cont=content if content else NA, i_str=f'{i}/{total_data}')
 
-        # Formatting:
-
-        http_i = max(link.find(HTTPS), link.find(HTTP))
-        if http_i != -1 and http_i != 0:
-            link = link[http_i:]  # not considering useless stuff before 'https://' (or 'http://')
-
-        elif link.startswith('/'):  # relative link
-            link = homepage + link
-
-        if not content:
-            content = NA
-
-        count += download(lnk=link, cont=content, i_str=f'{i}/{data_len}')
-
-    if downloaded:  # if at least one link is downloaded
+    if downloaded and skipped:  # if at least one link is downloaded and at least one link is skipped
 
         # SMART DOWNLOAD:
 
-        print('Smart Download: \n')
-        # list(map(print, downloaded)); print(); list(map(print, skipped)); print()  # raw
+        print('SMART DOWNLOAD: \n')
+        # list(map(print, downloaded)); print(); list(map(print, skipped)); print()  # debugging
 
         # Formatting Links:
 
@@ -195,11 +239,7 @@ while True:
 
         for i, link in enumerate(downloaded):
 
-            # Partitioning:
-            parts = link.split('/')[2:]  # 'https://example.com/' -> ['https:', '', 'example.com', ''] -> ['example.com', '']
-            if not parts[-1]:
-                parts.pop()  # ['example.com', ''] -> ['example.com']
-
+            parts = partitioned(link)
             downloaded[i] = parts
 
             # Counting Lengths:
@@ -211,18 +251,20 @@ while True:
 
         for i, (link, content) in enumerate(skipped):
 
-            # Partitioning:
-            parts = link.split('/')[2:]
-            if not parts[-1]:
-                parts.pop()
+            parts = partitioned(link)
 
-            if most_len-len(parts) == 1:  # TODO: this needs testing
+            # if len(parts) < most_len:
+            # len must be less than len of downloaded links by any NUMBER ✔
+            # more correct but complex because we don't even know that NUMBER here,
+            # e.g. skipped = 'example.com/bla' -> 'example.com/bla/free/download' = downloadable; so here NUMBER = 2
+            # so to make it straight assuming NUMBER = 1 -> e.g. -> skipped = 'example.com/bla' -> 'example.com/bla/download' = downloadable
+            if len(parts)+1 == most_len:
                 skipped[i] = (parts, content)
             else:
                 skipped[i] = []
 
         skipped = list(filter(None, skipped))
-        # list(map(print, downloaded)); print(); list(map(print, skipped)); print()  # processed
+        # list(map(print, downloaded)); print(); list(map(print, skipped)); print()  # debugging; partitioned
 
         # Classifying Links:
 
@@ -244,32 +286,28 @@ while True:
                     if c_part != part:
 
                         if not differed:
-
                             classifs[i][0][j] = ''  # generalizing
                             differed = True  # allowing one difference
 
                         else:
-
                             classifs = backup  # restore
                             not_classified = True
                             break  # unmatched classif as num_of_diffs > 1
 
                 if not not_classified:
-
                     classifs[i][1] += 1  # link classified
                     break  # as classified ✔
 
             if classifs == [] or not_classified:  # if no classifs are there or num_of_diffs > 1 everytime
-
                 classifs.append([parts, 1])  # new classif
 
         # list(map(print, classifs)); print()  # debugging
         generalized = sorted(classifs, key=lambda x: x[1], reverse=True)[0][0]
-        print('Generalized Download Link:', generalized, '\n')  # debugging
+        print('Generalized Download Link:', generalized, '\n\nTrying to match this format with the skipped links...\n')
 
         # Matching Generalized-Link Format:
 
-        smart_downloaded = []
+        matched = []
 
         for (parts, content) in skipped:  # loop over links
 
@@ -277,30 +315,30 @@ while True:
             changed = False
             not_matched = False
 
-            for g_part, part in zip(generalized, parts):  # loop to match generalized
+            for i in range(most_len):  # loop to match generalized
 
-                if g_part != part:
+                try:
+                    if parts[i] != generalized[i] and generalized[i] != '':
 
-                    if not changed:
-                        changed = True  # allowing one diff
+                        if not changed:
+                            parts.insert(i, generalized[i])
+                            changed = True  # allowing one diff
 
-                    else:
-                        not_matched = True
-                        break
+                        else:  # changes = 2
+                            not_matched = True
+                            break
 
-            if not not_matched:  # matched
+                except IndexError:  # due to parts[i] at last iteration, that's totally fine
+                    parts.append(generalized[i])
 
-                parts.append(generalized[-1])  # appending that one part which will make this skipped link to a downloadable link
-
-                if parts not in downloaded:  # ✔ link not already downloaded
-
-                    smart_downloaded.append((HTTPS+'/'.join(parts), content))
+            if not not_matched and parts not in downloaded:  # ✔ if matched and link not already downloaded
+                matched.append((HTTPS + '/'.join(parts), content))
 
         # Downloading Modified Links:
-        if smart_downloaded:
-            smart_dl_len = len(smart_downloaded)
-            for i, (link, content) in enumerate(iterable=smart_downloaded, start=1):
-                count += download(lnk=link, cont=content, i_str=f'{i}/{smart_dl_len}', smart=False)
+        if matched:
+            total_matched = len(matched)
+            for i, (link, content) in enumerate(iterable=matched, start=1):
+                count += download(link, cont=content, i_str=f'{i}/{total_matched}', smart=False)
         else:
             print('No Links Matched :( \n')
 
@@ -308,7 +346,3 @@ while True:
     chdir('..')  # back to home path
     if not count:  # deleting made dir if nothing's downloaded
         rmdir(dir_name)
-
-
-# TODO:
-#  Recursive Downloading to download requested no. of files.
