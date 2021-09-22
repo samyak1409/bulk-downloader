3caa91e720a7e06a268ee55f5a8004054aac5fe1
Samyak Jain <samyak65400@gmail.com>
2021-09-22 16:54:44 +0530

    smart download edge cases implemented + various improvements

--- Script.py
+++ Script.py
@@ -43,7 +43,7 @@ def sep_print(*msgs):
 
 
 def get_file_name(s: str):
-    """"""
+    """Takes a string s and returns its filename-possible version."""
     name = ''
     for char in s:
         name += char if char not in ('\\', '/', ':', '*', '?', '"', '<', '>', '|') else ' '  # a file name can't contain any of these characters
@@ -51,19 +51,18 @@ def get_file_name(s: str):
 
 
 def download(lnk: str, cont: str, i_str: str, smart: bool = True):
-    """"""
+    """Takes a link lnk, fetches its headers and downloads it if possible."""
 
     print(i_str+') Link:', lnk)
     print('Content Name:', cont)
 
     # Fetching Details:
 
-    try:
-        response = get_request(url=lnk, stream=True)
-        # print(response.headers, '\n')  # debugging
-    except RequestException as re:
-        print('ERROR:', re.__doc__, '\n')
+    response = get_response(lnk)
+    if response is None:  # (if function's except block executed)
+        print()  # spacing
         return 0  # skipping this link for any other further processing
+    # print(response.headers, '\n')  # debugging
 
     content_type = response.headers.get('Content-Type', NA)
     size = response.headers.get('Content-Length', NA)  # in B
@@ -100,7 +99,7 @@ def download(lnk: str, cont: str, i_str: str, smart: bool = True):
                     fd.write(chunk)
 
         except Exception as ex:  # any non-exit exception
-            print('ERROR OCCURRED:', ex, '\n')
+            print('ERROR:', ex, '\n')
             response.close()
             return 0
 
@@ -122,10 +121,17 @@ def download(lnk: str, cont: str, i_str: str, smart: bool = True):
         return 0
 
 
-def partitioned(lnk: str):  # 'https://example.com/'
-    """"""
-    p = lnk.split('/')[2:]  # ['https:', '', 'example.com', ''] -> ['example.com', '']
-    return p if p[-1] else p[:-1]  # ['example.com', ''] -> ['example.com']
+def get_response(lnk: str):
+    """Tries to get lnk response."""
+    try:
+        return get_request(url=lnk, stream=True)
+    except RequestException as re:
+        print('ERROR:', re.__doc__)
+
+
+def partitioned(lnk: str):  # 'https://example.com'
+    """Returns partitioned lnk."""
+    return lnk.split('/')[2:]  # ['https:', '', 'example.com'] -> ['example.com']
 
 
 # MAIN:
@@ -188,10 +194,9 @@ while True:
         # Anti-DDoS Detection:
         try:
             sep_print(f'OOPS! Entered URL is protected from bots by "{next(filter(lambda x: link_.startswith(x), anti_ddos))}". Please try entering some other website.')
+            continue
         except StopIteration:
             pass
-        else:
-            continue
 
         if link_.startswith('http://enable-javascript.com'):
             sep_print("OOPS! JavaScript webpage. Sorry I can't run JavaScript webpages, please try some other website.")
@@ -204,7 +209,7 @@ while True:
         sep_print()
         continue
 
-    if input('\nStart trying download (y/n): ').casefold() != 'y':  # confirming
+    if input('\nStart trying download (y/n): ').strip().casefold() != 'y':  # confirming
         sep_print('\n"Moved on."')
         continue
 
@@ -224,125 +229,167 @@ while True:
     downloaded, skipped = [], []  # downloaded links, skipped (link, content) tuple
 
     for i, (link, content) in enumerate(iterable=data.items(), start=1):  # iterating over fetched links
-        count += download(homepage+link if link.startswith(('/', '#')) else link, cont=content if content else NA, i_str=f'{i}/{total_data}')
+        if link in ('/', '#'):
+            continue  # else will form self pointing
+        elif link.startswith(('/', '#')):  # relative links
+            link = homepage + link
+        count += download(link[:-1] if link.endswith('/') else link, cont=content if content else NA, i_str=f'{i}/{total_data}')
 
-    if downloaded and skipped:  # if at least one link is downloaded and at least one link is skipped
+    # SMART DOWNLOAD:
 
-        # SMART DOWNLOAD:
+    if skipped:  # if at least one link is skipped
 
         print('SMART DOWNLOAD: \n')
         # list(map(print, downloaded)); print(); list(map(print, skipped)); print()  # debugging
 
-        # Formatting Links:
+        def dl_input(msg: str):
+            """Inputs download links from the user."""
+            print(msg)
+            for num in range(1, 3-len(downloaded)):
+                while True:
+                    lnk = input(f'{num}: ').strip()
+                    if not lnk:
+                        print("Nothing's entered. \n")
+                        return
+                    if lnk.endswith('/'):
+                        lnk = lnk[:-1]
+                    if not lnk.startswith(homepage):
+                        print('Please enter a link from the same website.')
+                        continue
+                    if lnk in downloaded:
+                        print('Link is already there, please enter a different link.')
+                        continue
+                    response = get_response(lnk)
+                    if response is None:  # (if function's except block executed)
+                        continue
+                    if 'html' in response.headers.get('Content-Type', NA):
+                        print('Link is webpage itself, please enter a link with some non-HTML content.')
+                        continue
+                    insort_right(a=downloaded, x=lnk)
+                    break
+            print()  # spacing
 
-        counts = {}
+        if len(downloaded) == 1:
+            dl_input(f'Hey User! Unfortunately only one file is downloaded ("{downloaded[0]}"), but Smart Download need at least 2 download links in order to work, so please go to "{url}", find a direct download link of the same type, copy it and paste here: ')
 
-        for i, link in enumerate(downloaded):
+        elif not downloaded:
+            dl_input(f'Hey User! Unfortunately no files were downloaded, but Smart Download need at least 2 download links in order to work, so please go to "{url}", find two download links, copy them and paste here one by one: ')
 
-            parts = partitioned(link)
-            downloaded[i] = parts
+        if len(downloaded) > 1:  # if at least two links are downloaded
 
-            # Counting Lengths:
-            parts_len = len(parts)
-            counts[parts_len] = counts.get(parts_len, 0) + 1
+            # Partitioning Downloaded Links:
 
-        most_len = sorted(counts.items(), key=lambda x: x[1], reverse=True)[0][0]  # length of most links (downloaded)
-        downloaded = list(filter(lambda x: len(x) == most_len, downloaded))
+            counts = {}
 
-        for i, (link, content) in enumerate(skipped):
+            for i, link in enumerate(downloaded):
 
-            parts = partitioned(link)
+                parts = partitioned(link)
+                downloaded[i] = parts
 
-            # if len(parts) < most_len:
-            # len must be less than len of downloaded links by any NUMBER ✔
-            # more correct but complex because we don't even know that NUMBER here,
-            # e.g. skipped = 'example.com/bla' -> 'example.com/bla/free/download' = downloadable; so here NUMBER = 2
-            # so to make it straight assuming NUMBER = 1 -> e.g. -> skipped = 'example.com/bla' -> 'example.com/bla/download' = downloadable
-            if len(parts)+1 == most_len:
-                skipped[i] = (parts, content)
+                # Counting Lengths:
+                parts_len = len(parts)
+                counts[parts_len] = counts.get(parts_len, 0) + 1
+
+            most_len = sorted(counts.items(), key=lambda x: x[1], reverse=True)[0][0]  # length of most links (downloaded)
+            downloaded = list(filter(lambda x: len(x) == most_len, downloaded))
+            # list(map(print, downloaded)); print()  # partitioned; debugging
+
+            # Classifying Links:
+
+            classifs = []  # classifications; [[parts (list), occurrence (int)], ...]
+
+            for parts in deepcopy(downloaded):  # loop over links; TODO: Why downloaded[0] is changing in this loop without deepcopy?
+
+                not_classified = None  # unnecessary line (added due to PyCharm's warning)
+
+                for i, classif in enumerate(classifs):  # loop over classifications
+
+                    backup = deepcopy(classifs)  # https://stackoverflow.com/questions/2612802/list-changes-unexpectedly-after-assignment-why-is-this-and-how-can-i-prevent-it; https://stackoverflow.com/questions/2465921/how-to-copy-a-dictionary-and-only-edit-the-copy
+                    differed = False
+                    not_classified = False
+
+                    for j, (c_part, part) in enumerate(zip(classif[0], parts)):  # loop to match a classification
+
+                        if c_part != part:
+
+                            if not differed:
+                                classifs[i][0][j] = ''  # generalizing
+                                differed = True  # allowing one difference
+
+                            else:
+                                classifs = backup  # restore
+                                not_classified = True
+                                break  # unmatched classif as num_of_diffs > 1
+
+                    if not not_classified:
+                        classifs[i][1] += 1  # link classified
+                        break  # as classified ✔
+
+                if classifs == [] or not_classified:  # if no classifs are there or num_of_diffs > 1 everytime
+                    classifs.append([parts, 1])  # new classif
+
+            # list(map(print, classifs)); print()  # debugging
+            generalized = sorted(classifs, key=lambda x: x[1], reverse=True)[0][0]
+            print('Generalized Download Link:', generalized, '\n\nTrying to match this format with the skipped links...\n')
+
+            # Partitioning Skipped Links:
+
+            for i, (link, content) in enumerate(skipped):
+
+                parts = partitioned(link)
+
+                # if len(parts) < most_len:
+                # len must be less than len of downloaded links by any NUMBER ✔
+                # more correct but complex because we don't even know that NUMBER here,
+                # e.g. skipped = 'example.com/bla' -> 'example.com/bla/free/download' = downloadable; so here NUMBER = 2
+                # so to make it straight assuming NUMBER = 1 -> e.g. -> skipped = 'example.com/bla' -> 'example.com/bla/download' = downloadable
+                if len(parts)+1 == most_len:
+                    skipped[i] = (parts, content)
+                else:
+                    skipped[i] = []
+
+            skipped = list(filter(None, skipped))
+            # list(map(print, skipped)); print()  # partitioned; debugging
+
+            # Matching Generalized-Link Format:
+
+            matched = []
+
+            for (parts, content) in skipped:  # loop over links
+
+                changed = False
+                not_matched = False
+
+                for i in range(most_len):  # loop to match generalized
+
+                    try:
+                        if parts[i] != generalized[i] and generalized[i] != '':
+
+                            if not changed:
+                                parts.insert(i, generalized[i])
+                                changed = True  # allowing one diff
+
+                            else:  # changes = 2
+                                not_matched = True
+                                break
+
+                    except IndexError:  # due to parts[i] at last iteration, that's totally fine
+                        parts.append(generalized[i])
+
+                if not not_matched and parts not in downloaded:  # ✔ if matched and link not already downloaded
+                    matched.append((HTTPS + '/'.join(parts), content))
+
+            # Downloading Modified Links:
+            if matched:
+                total_matched = len(matched)
+                for i, (link, content) in enumerate(iterable=matched, start=1):
+                    count += download(link, cont=content, i_str=f'{i}/{total_matched}', smart=False)
             else:
-                skipped[i] = []
-
-        skipped = list(filter(None, skipped))
-        # list(map(print, downloaded)); print(); list(map(print, skipped)); print()  # debugging; partitioned
-
-        # Classifying Links:
-
-        classifs = []  # classifications; [[parts (list), occurrence (int)], ...]
-
-        for parts in deepcopy(downloaded):  # loop over links; TODO: Why downloaded[0] is changing in this loop without deepcopy?
-
-            # print(parts)  # debugging
-            not_classified = None  # unnecessary line (added due to PyCharm's warning)
-
-            for i, classif in enumerate(classifs):  # loop over classifications
-
-                backup = deepcopy(classifs)  # https://stackoverflow.com/questions/2612802/list-changes-unexpectedly-after-assignment-why-is-this-and-how-can-i-prevent-it; https://stackoverflow.com/questions/2465921/how-to-copy-a-dictionary-and-only-edit-the-copy
-                differed = False
-                not_classified = False
-
-                for j, (c_part, part) in enumerate(zip(classif[0], parts)):  # loop to match a classification
-
-                    if c_part != part:
-
-                        if not differed:
-                            classifs[i][0][j] = ''  # generalizing
-                            differed = True  # allowing one difference
-
-                        else:
-                            classifs = backup  # restore
-                            not_classified = True
-                            break  # unmatched classif as num_of_diffs > 1
-
-                if not not_classified:
-                    classifs[i][1] += 1  # link classified
-                    break  # as classified ✔
-
-            if classifs == [] or not_classified:  # if no classifs are there or num_of_diffs > 1 everytime
-                classifs.append([parts, 1])  # new classif
-
-        # list(map(print, classifs)); print()  # debugging
-        generalized = sorted(classifs, key=lambda x: x[1], reverse=True)[0][0]
-        print('Generalized Download Link:', generalized, '\n\nTrying to match this format with the skipped links...\n')
-
-        # Matching Generalized-Link Format:
-
-        matched = []
-
-        for (parts, content) in skipped:  # loop over links
-
-            # print(parts)  # debugging
-            changed = False
-            not_matched = False
-
-            for i in range(most_len):  # loop to match generalized
-
-                try:
-                    if parts[i] != generalized[i] and generalized[i] != '':
-
-                        if not changed:
-                            parts.insert(i, generalized[i])
-                            changed = True  # allowing one diff
-
-                        else:  # changes = 2
-                            not_matched = True
-                            break
-
-                except IndexError:  # due to parts[i] at last iteration, that's totally fine
-                    parts.append(generalized[i])
-
-            if not not_matched and parts not in downloaded:  # ✔ if matched and link not already downloaded
-                matched.append((HTTPS + '/'.join(parts), content))
-
-        # Downloading Modified Links:
-        if matched:
-            total_matched = len(matched)
-            for i, (link, content) in enumerate(iterable=matched, start=1):
-                count += download(link, cont=content, i_str=f'{i}/{total_matched}', smart=False)
-        else:
-            print('No Links Matched :( \n')
+                print('No Links Matched :( \n')
 
     sep_print('Files Downloaded:', count)
     chdir('..')  # back to home path
-    if not count:  # deleting made dir if nothing's downloaded
-        rmdir(dir_name)
+    try:
+        rmdir(dir_name)  # deleting made dir if nothing's downloaded
+    except OSError:
+        pass
